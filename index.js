66cd5e1ce58cc1b93243b9f5fa2b11d26d7e3bb3
jternard <jeremy.ternard@gmail.com>
2024-12-19 15:31:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -111,5 +111,5 @@ function newWord() {
 
 function checkArticle(caller) {
   var btnArticle = caller.id.substr(-3).toLowerCase();
-  btnArticle == mainArticle ? caller.className = "btn btn-outline-success" : caller.className = "btn btn-outline-danger";
+  (btnArticle === mainArticle) ? caller.className = "btn btn-outline-success" : caller.className = "btn btn-outline-danger";
 }
