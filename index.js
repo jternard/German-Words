0cd26ab115ea996ec28ae451c1c70b60f6f89616
jternard <jeremy.ternard@gmail.com>
2024-12-19 08:55:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const myHeading = document.querySelector("h1");
 
 function rand(number) {
-  return truncate(Math.random(number);
+  return Math.trunc(Math.random(number);
 }
 
 myHeading.textContent = "Hello world! " + rand(200);
